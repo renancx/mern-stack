81db6eab3334962861df88302106b759a40e04d5
Renan <renan.lowe@hotmail.com>
2024-11-23 18:11:31 -0300

    feat: implement product management API with CRUD operations

---
+++ server/controllers/ProductController.js
@@ -0,0 +1,56 @@
+import mongoose from 'mongoose';
+import Product from '../models/ProductModel.js';
+
+export const getProducts = async (req, res) => {
+    try {
+        const products = await Product.find({});
+        return res.status(200).json({success: true, data: products});
+    } catch (error) {
+        return res.status(500).json({success: false, message: "Server error"});
+    }
+};
+
+export const createProduct = async (req, res) => {
+    const product = req.body; //user input
+
+    if (!product.name || !product.price || !product.image) {
+        return res.status(400).json({success: false, message: 'All fields are required' });
+    }
+
+    const newProduct = new Product(product);
+
+    try {
+        await newProduct.save();
+        return res.status(201).json({success: true, data: newProduct});
+    } catch (error) {
+        return res.status(500).json({success: false, message: "Server error"});
+    }
+};
+
+export const updateProduct =  async (req, res) => {
+    const {id} = req.params;
+    const product = req.body;
+
+    if(!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({success: false, message: 'Invalid product id'});
+    }
+
+    try {
+        const updatedProduct = await Product.findByIdAndUpdate
+        (id, product, {new: true});
+        res.status(200).json({success: true, data: updatedProduct});
+    } catch (error) {
+        res.status(500).json({success: false, message: 'Server error'});
+    }
+};
+
+export const deleteProduct = async (req, res) => {
+    const {id} = req.params;
+
+    try {
+        await Product.findByIdAndDelete(id);
+        res.status(200).json({success: true, message: 'Product deleted successfully'});
+    } catch (error) {
+        res.status(404).json({success: false, message: 'Product not found'});
+    }
+};
--- server/server.js
+++ server/server.js
@@ -1,13 +1,16 @@
 import express from 'express';
-import bodyParser from "body-parser";
 import dotenv from "dotenv";
 import { connectDB } from './config/db.js';
+import ProductRoute from './routes/ProductRoute.js';
 
 const app = express();
-app.use(bodyParser.json());
+app.use(express.json());
+
 dotenv.config();
+
+app.use('/api/products', ProductRoute);
 
 app.listen(process.env.PORT, () => {
     connectDB();
-    console.log(`Server is running on port ${process.env.PORT}`);
+    console.log('Server is running on port ' + process.env.PORT);
 });
