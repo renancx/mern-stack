dd7f862ba000827d62b7bb15aa1da1da66f7db98
renancx <renan.lowe@hotmail.com>
2025-01-09 14:48:52 -0300

    feat: update NotFound page

--- client/src/pages/NotFound/NotFound.jsx
+++ client/src/pages/NotFound/NotFound.jsx
@@ -1,13 +1,16 @@
-import styles from "./styles.module.scss";    
+import styles from "./styles.module.scss";
 import Navbar from "../../components/Navbar/Navbar";
 import Footer from "../../components/Footer/Footer";
+import error404 from "../../images/404.svg";
 
 export default function NotFound() {
 
     return (
         <div className={styles["container"]}>
             <Navbar />
-            
+                <img src={error404} alt="404" className={styles["img"]} width={550} height={550} />
+
+                <button className={styles["btn"]} onClick={() => window.location.href = "/"}>Go back to Home</button>
             <Footer />
         </div>
     )
